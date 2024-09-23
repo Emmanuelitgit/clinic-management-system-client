ccbebff50fcb6a285d08851f738613718258f9c9
Emmanuelitgit <eyidana001@st.ug.edu.gh>
2024-09-23 21:58:37 +0000

    report

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://clinic-server-o79p.onrender.com',
-  baseURL: '  http://localhost:26651',
+  baseURL: 'https://clinic-server-o79p.onrender.com',
+  // baseURL: '  http://localhost:26651',
   headers: {
     'Content-Type': 'application/json',
   },
